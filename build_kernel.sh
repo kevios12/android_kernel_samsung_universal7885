685afa7df12ed283e87c78819efdf83a4d33ba1f
kevios12 <kerschbaumer.kevin2@gmail.com>
2024-02-25 03:22:17 +0100

    kernel: build_script: Add and Update more Stuff
    * Add Auto Build
    * change Colors from different functions
    * and more ...

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Exports
-export KBUILD_BUILD_USER="Kevios12"
-export KBUILD_BUILD_HOST="ubuntu"
+
+# Export build variables
+export KBUILD_BUILD_USER=""
+export KBUILD_BUILD_HOST=""
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 
@@ -12,8 +13,10 @@ declare -g ZIP_FILENAME
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
 NC='\033[0m'
 
+# Compiler
 TOOLCHAIN="$SRCTREE/toolchain/bin"
 LLVM_DIS_ARGS="llvm-dis AR=llvm-ar AS=llvm-as NM=llvm-nm LD=ld.lld OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip"
 TRIPLE="aarch64-linux-gnu-"
@@ -30,34 +33,129 @@ CHAT_ID=""
 BOT_TOKEN=""
 TELEGRAM_API="https://api.telegram.org/bot${BOT_TOKEN}"
 
-# We are starting with Toolchain check
+# Initialize
 init() {
+	clear
+	if [ -z "$BOT_TOKEN" ] || [ -z "$CHAT_ID"]; then
+		echo -e "${YELLOW}!!!Your BOT Token or Chat ID is Empty!!!${NC}\n"
+		sleep 3
+		clear
+		echo -e "${GREEN}Do u want to start the Auto Builder?${NC}\n"
+		echo -e "${RED}If u say Yes, Please configure Telegram!${NC}\n"
+		echo -e "${YELLOW}Warning: Everything will be Cleaned up inclusive Out/Toolchain Folder!${NC}\n"
+	else
+		clear
+		echo -e "${GREEN}Do u want to start the Auto Builder?${NC}\n"
+		echo -e "${RED}If u say Yes, Please configure Telegram!${NC}\n"
+		echo -e "${YELLOW}Warning: Everything will be Cleaned up inclusive Out/Toolchain Folder!${NC}\n"
+
+	fi
+	select auto in "Yes" "No" "Exit"; do
+		case "$auto" in
+		"Yes")
+			clear
+			rm -rf toolchain
+			rm -rf out && make clean && make mrproper
+			clear
+			if [[ "$DISTRO" == "Ubuntu 23.10" ]]; then
+				echo -e "${YELLOW}Downloading Neutron-Clang Toolchain ...${NC}\n"
+				mkdir -p "$HOME/toolchains/neutron-clang"
+				cd "$HOME/toolchains/neutron-clang"
+				bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S
+				cd "$SRCTREE"
+				cp -r "$HOME/toolchains/neutron-clang" toolchain
+				clear
+			elif [[ "$DISTRO" == "Ubuntu 21.10" ]]; then
+				echo -e "${YELLOW}Downloading Vortex-Clang Toolchain ...${NC}\n"
+				git clone --depth=1 https://github.com/vijaymalav564/vortex-clang toolchain
+				clear
+			elif [[ "$DISTRO" == "Ubuntu 20.04.6 LTS" ]]; then
+				echo -e "${YELLOW}Downloading Proton-Clang Toolchain ...${NC}\n"
+				git clone --depth=1 https://github.com/kdrag0n/proton-clang toolchain
+				clear
+			fi
+			auto
+			echo -e "${GREEN}Uploading: $SRCTREE/kernel_zip/anykernel/$ZIP_FILENAME to Telegram ...${NC}\n"
+			file_size_mb=$(stat -c "%s" "$SRCTREE/kernel_zip/anykernel/${ZIP_FILENAME}" | awk '{printf "%.2f", $1 / (1024 * 1024)}')
+			curl -s -X POST "${TELEGRAM_API}/sendMessage" -d "chat_id=${CHAT_ID}" -d "text=Auto Builder%0AUploading: ${ZIP_FILENAME}%0ASize: ${file_size_mb}MB%0ABuild Date: $(date +'%Y-%m-%d %H:%M %Z')"
+			curl -s -F chat_id="${CHAT_ID}" -F document=@"$SRCTREE/kernel_zip/anykernel/${ZIP_FILENAME}" "${TELEGRAM_API}/sendDocument"
+			clear
+			echo -e "${GREEN}Done ... Exiting ...${NC}\n"
+			exit
+			;;
+		"No")
+			start
+			toolchain
+			break
+			;;
+		"Exit")
+			clear
+			echo -e "${RED}Telegram Upload skipped. Exiting ...${NC}\n"
+			exit
+			;;
+		*)
+			echo -e "${RED}Invalid option. Please select again.${NC}\n"
+			break
+			;;
+		esac
+	done
+}
+
+start() {
+	clear
+	echo -e "${GREEN}Do u want to make Clean Build?${NC}\n"
+	select clean in "Yes" "No" "Exit"; do
+		case "$clean" in
+		"Yes")
+			echo -e "${YELLOW}Cleaning up ...${NC}\n"
+			rm -rf out && make clean && make mrproper
+			toolchain
+			break
+			;;
+		"No")
+			toolchain
+			break
+			;;
+		"Exit")
+			clear
+			echo -e "${RED}Exiting ...${NC}\n"
+			break
+			;;
+		*)
+			echo -e "${RED}Invalid option. Please select again.${NC}\n"
+			;;
+		esac
+	done
+}
+
+# Let's do a Toolchain check
+toolchain() {
 	if [ -d "$TOOLCHAIN" ]; then
-		echo -e "${GREEN}Looks like you already have a Toolchain.${NC}"
-		echo -e "${YELLOW}Do you want to continue and start the Build?${NC}"
+		clear
+		echo -e "${GREEN}Looks like you already have a Toolchain.${NC}\n"
+		echo -e "${YELLOW}Do you want to continue and start the Build?${NC}\n"
 		select choice in "Continue" "Exit"; do
 			case "$choice" in
 			"Continue")
+				clear
 				make_defconfig
 				break
 				;;
 			"Exit")
-				echo -e "${RED}Exiting.${NC}"
+				clear
+				echo -e "${RED}Exiting ...${NC}\n"
 				exit
 				;;
 			*)
-				echo -e "${RED}Invalid option. Please select again.${NC}"
+				echo -e "${RED}Invalid option. Please select again.${NC}\n"
 				;;
 			esac
 		done
 	else
-		echo -e "${RED}No Toolchain found!${NC}"
-		echo ""
-		echo -e "${YELLOW}Warning: Toolchains need different Operating Systems!${NC}"
-		echo -e "${YELLOW}Check below if your System are compatible before u Continue.${NC}"
-		echo ""
-		echo -e "${GREEN}Your current OS is: $DISTRO - GLIBC: $GLIBC_VERSION${NC}"
-		echo ""
+		echo -e "${RED}No Toolchain found!${NC}\n"
+		echo -e "${YELLOW}Warning: Toolchains need different Operating Systems!${NC}\n"
+		echo -e "${YELLOW}Check below if your System are compatible before u Continue.${NC}\n"
+		echo -e "${GREEN}Your current OS is: $DISTRO - GLIBC: $GLIBC_VERSION${NC}\n"
 		if [[ "$DISTRO" == "Ubuntu 23.10" ]]; then
 			echo -e "${YELLOW}➜ Neutron needs ${GREEN}Ubuntu 23.10 [GLIBC2.38]${NC}"
 			echo -e "${YELLOW}Vortex needs ${RED}Ubuntu 21.10 [GLIBC2.34]${NC}"
@@ -72,34 +170,38 @@ init() {
 			echo -e "${YELLOW}➜ Proton needs ${GREEN}Ubuntu 20.04.6 LTS [GLIBC2.31]${NC}"
 		fi
 		echo ""
-		echo -e "${GREEN}Proceed to Download the Toolchain?${NC}"
-		select toolchain in "Yes" "Quit"; do
+		echo -e "${GREEN}Proceed to Download the Toolchain?${NC}\n"
+		select toolchain in "Yes" "Exit"; do
 			case $toolchain in
 			"Yes")
 				if [[ "$DISTRO" == "Ubuntu 23.10" ]]; then
-					echo -e "${YELLOW}Downloading Neutron-Clang Toolchain ...${NC}"
+					echo -e "${YELLOW}Downloading Neutron-Clang Toolchain ...${NC}\n"
 					mkdir -p "$HOME/toolchains/neutron-clang"
 					cd "$HOME/toolchains/neutron-clang"
 					bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S
 					cd "$SRCTREE"
 					cp -r "$HOME/toolchains/neutron-clang" toolchain
+					clear
 					make_defconfig
 				elif [[ "$DISTRO" == "Ubuntu 21.10" ]]; then
-					echo -e "${YELLOW}Downloading Vortex-Clang Toolchain ...${NC}"
+					echo -e "${YELLOW}Downloading Vortex-Clang Toolchain ...${NC}\n"
 					git clone --depth=1 https://github.com/vijaymalav564/vortex-clang toolchain
+					clear
 					make_defconfig
 				elif [[ "$DISTRO" == "Ubuntu 20.04.6 LTS" ]]; then
-					echo -e "${YELLOW}Downloading Proton-Clang Toolchain ...${NC}"
+					echo -e "${YELLOW}Downloading Proton-Clang Toolchain ...${NC}\n"
 					git clone --depth=1 https://github.com/kdrag0n/proton-clang toolchain
+					clear
 					make_defconfig
 				fi
 				;;
-			"Quit")
-				echo -e "${RED}Exiting ...${NC}"
+			"Exit")
+				clear
+				echo -e "${RED}Exiting ...${NC}\n"
 				exit
 				;;
 			*)
-				echo -e "${RED}Invalid option. Please select again.${NC}"
+				echo -e "${RED}Invalid option. Please select again.${NC}\n"
 				;;
 			esac
 		done
@@ -107,11 +209,14 @@ init() {
 }
 
 make_defconfig() {
+	echo -e "${BLUE}"
 	make O=out ARCH=arm64 exynos7885-a40_defconfig
+	echo -e "${NC}"
 	build_kernel
 }
 
 build_kernel() {
+	echo -e "${BLUE}"
 	PATH=$TOOLCHAIN:$PATH \
 		make O=out -j$(nproc --all) \
 		ARCH=arm64 \
@@ -122,7 +227,10 @@ build_kernel() {
 		CLANG_TRIPLE=$TRIPLE \
 		CROSS_COMPILE=$CROSS \
 		CROSS_COMPILE_ARM32=$CROSS_ARM32
-	upload
+	echo -e "${NC}"
+	clear
+	echo -e "${YELLOW}Creating ZIP ...${NC}\n"
+	pack
 }
 
 create_zip() {
@@ -148,8 +256,6 @@ create_zip() {
 	sed -i "s/Build Date: .*/Build Date: $(date +'%Y-%m-%d %H:%M %Z')/g" "version"
 	ZIP_FILENAME="Kernel_A40_${NEXT_NUMBER}_[$VERSION.$PATCHLEVEL.$SUBLEVEL].zip"
 	zip -r9 "$ZIP_FILENAME" "$@"
-	echo ""
-	echo "Zip file created: $ZIP_FILENAME and saved in $SRCTREE/kernel_zip/anykernel/"
 }
 
 pack() {
@@ -158,32 +264,63 @@ pack() {
 	cd $SRCTREE/kernel_zip/anykernel/
 	create_zip META-INF tools anykernel.sh Image version
 	cd $SRCTREE
+	clear
+	echo -e "${GREEN}Zip file created: $ZIP_FILENAME and saved in $SRCTREE/kernel_zip/anykernel/${NC}\n"
+	echo -e "${GREEN}***************************************************"
+	echo "	          Kernel: $VERSION.$PATCHLEVEL.$SUBLEVEL"
+	echo "          Build Date: $(date +'%Y-%m-%d %H:%M %Z')"
+	echo -e "***************************************************${NC}\n"
+	echo -e "${GREEN}Upload to Telegram?${NC}\n"
+	tg_upload
 }
 
-upload() {
-	pack
-	tg_upload
+auto() {
+	echo -e "${BLUE}"
+	make O=out ARCH=arm64 exynos7885-a40_defconfig
+	echo -e "${NC}"
+	echo -e "${BLUE}"
+	PATH=$TOOLCHAIN:$PATH \
+		make O=out -j$(nproc --all) \
+		ARCH=arm64 \
+		LLVM_DIS=$LLVM_DIS_ARGS \
+		LLVM=1 \
+		CC=clang \
+		LD_LIBRARY_PATH="$LD:$LD_LIBRARY_PATH" \
+		CLANG_TRIPLE=$TRIPLE \
+		CROSS_COMPILE=$CROSS \
+		CROSS_COMPILE_ARM32=$CROSS_ARM32
+	echo -e "${NC}"
+	clear
+	echo -e "${YELLOW}Creating ZIP ...${NC}\n"
+	cp out/arch/arm64/boot/Image kernel_zip/anykernel/
+	cd $SRCTREE/kernel_zip/anykernel/
+	create_zip META-INF tools anykernel.sh Image version
+	cd $SRCTREE
 }
 
 tg_upload() {
 	select telegram in "Yes" "No"; do
 		case "$telegram" in
 		"Yes")
-			echo -e "${GREEN}Uploading $SRCTREE/kernel_zip/anykernel/$ZIP_FILENAME to Telegram ... ${NC}"
+			echo -e "${GREEN}Uploading $SRCTREE/kernel_zip/anykernel/$ZIP_FILENAME to Telegram ...${NC}"
 			file_size_mb=$(stat -c "%s" "$SRCTREE/kernel_zip/anykernel/${ZIP_FILENAME}" | awk '{printf "%.2f", $1 / (1024 * 1024)}')
-			curl -s -X POST "${TELEGRAM_API}/sendMessage" -d "chat_id=${CHAT_ID}" -d "text=Uploading: ${ZIP_FILENAME}%0ASize: ${file_size_mb}MB"
+			curl -s -X POST "${TELEGRAM_API}/sendMessage" -d "chat_id=${CHAT_ID}" -d "text=Uploading: ${ZIP_FILENAME}%0ASize: ${file_size_mb}MB%0ABuild Date: $(date +'%Y-%m-%d %H:%M %Z')"
 			curl -s -F chat_id="${CHAT_ID}" -F document=@"$SRCTREE/kernel_zip/anykernel/${ZIP_FILENAME}" "${TELEGRAM_API}/sendDocument"
+			clear
 			break
 			;;
 		"No")
-			echo "Telegram Upload skipped."
-			break
+			clear
+			echo -e "${RED}Telegram Upload skipped. Exiting ...${NC}\n"
+			echo -e "${GREEN}ZIP Output: $SRCTREE/kernel_zip/anykernel/${ZIP_FILENAME}${NC}\n"
+			exit
 			;;
 		*)
-			echo "Invalid option. Please select again."
+			echo -e "${RED}Invalid option. Please select again.${NC}\n"
 			;;
 		esac
 	done
 }
 
 init
+
