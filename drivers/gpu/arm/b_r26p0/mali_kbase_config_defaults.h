29d1ac9a54d88819fa7555b93a7384b2c0e099a6
ace2nutzer <>
2024-05-25 13:13:27 +0200

    drivers/gpu/b_r26p0/mali: relax aggresive powersaving

--- drivers/gpu/arm/b_r26p0/mali_kbase_config_defaults.h
+++ drivers/gpu/arm/b_r26p0/mali_kbase_config_defaults.h
@@ -101,7 +101,7 @@ enum {
 /**
  * Default period for DVFS sampling
  */
-#define DEFAULT_PM_DVFS_PERIOD 100 /* 100ms */
+#define DEFAULT_PM_DVFS_PERIOD 20 /* 100ms */
 
 /**
  * Power Management poweroff tick granuality. This is in nanoseconds to
@@ -111,17 +111,17 @@ enum {
  * -# Power off one or more shader cores
  * -# Power off the entire GPU
  */
-#define DEFAULT_PM_GPU_POWEROFF_TICK_NS (400000) /* 400us */
+#define DEFAULT_PM_GPU_POWEROFF_TICK_NS (4000000) /* 4000us */
 
 /**
  * Power Manager number of ticks before shader cores are powered off
  */
-#define DEFAULT_PM_POWEROFF_TICK_SHADER (2) /* 400-800us */
+#define DEFAULT_PM_POWEROFF_TICK_SHADER (20) /* 4000-8000us */
 
 /**
  * Default scheduling tick granuality
  */
-#define DEFAULT_JS_SCHEDULING_PERIOD_NS    (100000000u) /* 100ms */
+#define DEFAULT_JS_SCHEDULING_PERIOD_NS    (20000000u) /* 20ms */
 
 /**
  * Default minimum number of scheduling ticks before jobs are soft-stopped.
@@ -129,12 +129,12 @@ enum {
  * This defines the time-slice for a job (which may be different from that of a
  * context)
  */
-#define DEFAULT_JS_SOFT_STOP_TICKS       (1) /* 100ms-200ms */
+#define DEFAULT_JS_SOFT_STOP_TICKS       (10) /* 1000ms-2000ms */
 
 /**
  * Default minimum number of scheduling ticks before CL jobs are soft-stopped.
  */
-#define DEFAULT_JS_SOFT_STOP_TICKS_CL    (1) /* 100ms-200ms */
+#define DEFAULT_JS_SOFT_STOP_TICKS_CL    (10) /* 1000ms-2000ms */
 
 /**
  * Default minimum number of scheduling ticks before jobs are hard-stopped
@@ -191,7 +191,7 @@ enum {
  * @note the resolution is nanoseconds (ns) here, because that's the format
  * often used by the OS.
  */
-#define DEFAULT_JS_CTX_TIMESLICE_NS (50000000) /* 50ms */
+#define DEFAULT_JS_CTX_TIMESLICE_NS (20000000) /* 20ms */
 
 /**
  * Maximum frequency (in kHz) that the GPU can be clocked. For some platforms
