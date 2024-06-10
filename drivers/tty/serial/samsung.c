bf35579a0f60123f37c2deb470023fa9852d4152
kevios12 <kerschbaumer.kevin2@gmail.com>
2024-06-10 15:45:23 +0000

    Revert "tty: serial: samsung_tty: Fix a memory leak in s3c24xx_serial_getclk() in case of error"
    This reverts commit 719212c8de5001b2ccc0084e86600cc09fcfafcd.

--- drivers/tty/serial/samsung.c
+++ drivers/tty/serial/samsung.c
@@ -1006,13 +1006,8 @@ static unsigned int s3c24xx_serial_getclk(struct s3c24xx_uart_port *ourport,
 		if (ourport->dbg_mode & UART_DBG_MODE)
 			printk(" - Clock rate : %ld\n", rate);
 
-		rate = clk_get_rate(clk);
-		if (!rate) {
-			dev_err(ourport->port.dev,
-				"Failed to get clock rate for %s.\n", clkname);
-			clk_put(clk);
+		if (!rate)
 			continue;
-		}
 
 		if (ourport->info->has_divslot) {
 			unsigned long div = rate / req_baud;
