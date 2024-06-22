a1c26060fec2a34325ef33e10bfe58ae8602b735
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2024-06-23 01:22:13 +0200

    mm: Import memory management hacks from Oplus This imports a couple of memory management hacks used by Oplus (Oppo, Realme, and OnePlus) on their recent devices. This patch does the following:
    - Add a separate swappiness (default 60) value to use if task isn't handled by kswapd
    - Don't throttle tasks with OOM value < 0
    - Increase zsmalloc's maximum page order
    
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>
    [TenSeventy7: Make the Kconfig more descriptive]
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -149,6 +149,12 @@ struct scan_control {
  * From 0 .. 100.  Higher means more swappy.
  */
 int vm_swappiness = 60;
+#ifdef CONFIG_OPLUS_MM_HACKS
+/*
+ * Direct reclaim swappiness, exptct 0 - 60. Higher means more swappy and slower.
+ */
+int direct_vm_swappiness = 60;
+#endif /*CONFIG_OPLUS_MM_HACKS*/
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
@@ -1633,6 +1639,10 @@ putback_inactive_pages(struct lruvec *lruvec, struct list_head *page_list)
  */
 static int current_may_throttle(void)
 {
+#ifdef CONFIG_OPLUS_MM_HACKS
+	if ((current->signal->oom_score_adj < 0))
+		return 0;
+#endif /*CONFIG_OPLUS_MM_HACKS*/
 	return !(current->flags & PF_LESS_THROTTLE) ||
 		current->backing_dev_info == NULL ||
 		bdi_write_congested(current->backing_dev_info);
@@ -2276,6 +2286,9 @@ static void get_scan_count(struct lruvec *lruvec, int swappiness,
 	enum lru_list lru;
 	bool some_scanned;
 	int pass;
+#ifdef CONFIG_OPLUS_MM_HACKS
+	unsigned long totalswap = total_swap_pages;
+#endif /*CONFIG_OPLUS_MM_HACKS*/
 
 	/*
 	 * If the zone or memcg is small, nr[l] can be 0.  This
@@ -2296,8 +2309,14 @@ static void get_scan_count(struct lruvec *lruvec, int swappiness,
 	if (!global_reclaim(sc))
 		force_scan = true;
 
+#ifdef CONFIG_OPLUS_MM_HACKS
+	if (!current_is_kswapd())
+		swappiness = direct_vm_swappiness;
+	if (!sc->may_swap || (get_nr_swap_pages() <= totalswap>>6)) {
+#else
 	/* If we have no swap space, do not bother scanning anon pages. */
 	if (!sc->may_swap || (get_nr_swap_pages() <= 0)) {
+#endif /*CONFIG_OPLUS_MM_HACKS*/
 		scan_balance = SCAN_FILE;
 		goto out;
 	}
